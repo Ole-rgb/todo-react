ddb10711c7932f11d46be1c965dbb91017f52196
Ole <ole.nelson.r@gmail.com>
2023-02-26 17:10:07 +0100

    implemented AutoProvider

---
+++ src/components/context/AuthProvider.js
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext({}); //empty context
+
+//hands the value (aka the AuthState to all children)
+export const AuthProvider = ({children}) =>{ //children are the components nested in the provider
+    const [auth, setAuth] = useState({});
+
+    return(
+        <AuthContext.Provider value={{auth,setAuth}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext;
