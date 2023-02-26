0f9d7103b5a547d4401d1c0c53b76c4f804a254e
Ole <ole.nelson.r@gmail.com>
2023-02-26 17:12:10 +0100

    implemented ReactLinks

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -2,6 +2,7 @@ import React from "react";
 import Nav from "react-bootstrap/Nav"
 import Container from "react-bootstrap/Container"
 import Navbar from "react-bootstrap/Navbar"
+import { Link } from "react-router-dom";
 // import NavDropdown from "react-bootstrap/NavDropdown"
 import "bootstrap/dist/css/bootstrap.min.css"
 
@@ -11,11 +12,11 @@ function NavbarComponent() {
         <>
             <Navbar bg="light" expand="lg">
                 <Container>
-                    <Navbar.Brand href="/">TODOLIST</Navbar.Brand>
+                    <Link className="navbar-brand" to="/">TODOLIST</Link>
                     <Nav className="me-auto">
-                        <Nav.Link href="/todo/all">SHOW_TODOS</Nav.Link>
-                        <Nav.Link href="/todo/add">ADD_TODO</Nav.Link>
-                        <Nav.Link href="/login">LOGIN</Nav.Link>
+                        <Link to="/todo/all" className="nav-link">SHOW_TODOS</Link>
+                        <Link to="/todo/add" className="nav-link">ADD_TODO</Link>
+                        <Link to="/accounts/login" className="nav-link">LOGIN</Link>
                     </Nav>
                 </Container>
             </Navbar>
